9c5c1a6bb78d62dbdb5e577c40bba0db588b864e
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2021-06-02 00:57:04 +0530

    learnt how to use props

--- bgChanger/App.js
+++ bgChanger/App.js
@@ -1,19 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   View,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  StatusBar
 } from 'react-native';
 
 const App = () => {
 
   const [randomColor, setRandomColor] = useState('rgb(30, 30, 30)')
   
+  const changeBackground = () => {
+    let color = 'rgb(' +
+      Math.floor(Math.random() * 256) +
+      ','+
+      Math.floor(Math.random() * 256) +
+      ','+
+      Math.floor(Math.random() * 256) +
+    ')';
+    
+    setRandomColor(color);
+  }
+
+  const resetBg = () => {
+    setRandomColor('rgb(30, 30, 30)')
+  }
+
   return(
     <>
+      <StatusBar backgroundColor={randomColor} />
       <View style={[{backgroundColor: randomColor}, styles.container]}>
-        <Text style={styles.text}>Change Background</Text>
+        <TouchableOpacity onPress={changeBackground}>
+          <Text style={styles.text}>Change Background</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={resetBg}>
+          <Text style={[styles.text, {marginTop:15, backgroundColor: 'red'}]}>
+            Reset
+          </Text>
+        </TouchableOpacity>
       </View>
     </>
   );
