1990ed4bf2b70847da735ea78df2b481212c53c9
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2021-06-02 00:03:32 +0530

    learnt how to add styling

--- bgChanger/App.js
+++ bgChanger/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import {
-  Text
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native';
 
 const App = () => {
   return(
     <>
-      <Text>Atchyut Nagabhairava</Text>
+      <View style={[{backgroundColor: '#000000'}, styles.container]}>
+        <Text style={styles.text}>Change Background</Text>
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    textTransform: 'uppercase',
+    borderRadius: 15,
+    backgroundColor: 'green'
+  }
+})
 
 export default App;
